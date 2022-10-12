9790ce3220982cbf56d9d5bc3b60f5c43d1c1378
Panagiotis Favvatas <pfavvatas@yahoo.com>
2022-10-12 15:31:47 +0300

    ubuntu installer

---
+++ ubuntu-installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+#export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+sudo apt-get install nodejs npm
+sudo apt install npm
+sudo npm install expo
+sudo npm install --global expo-cli
+npm install -g sharp-cli
+sudo apt-get update
